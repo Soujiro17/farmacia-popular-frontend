46132cb5edb3d41dbf205cf099e1101fbf17cf83
CarlosEscobedo-afk <carlos.escobedo@alumnos.uv.cl>
2022-12-17 17:23:21 -0300

    Creacion formato

---
+++ src/components/Seleccion/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+
+export default function Seleccion() {
+  return (
+    <div>
+      <div className={styles.centerConteiner}>
+        <div className={styles.line} />
+        <div className={styles.groupButton}>
+          <button type="button">Ingresar pedido</button>
+          <button type="button">Visualizacion</button>
+        </div>
+      </div>
+      <img src="/img/icono.png" alt="Icono" className={styles.conteinerImg} />
+    </div>
+  );
+}
