b38af0102d87143d78c4f9321a871d3c74ce0ede
CarlosEscobedo-afk <carlos.escobedo@alumnos.uv.cl>
2022-12-17 21:15:18 -0300

    Añadir link a formulario y añadir ruta

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Inicio from "../pages/Inicio";
+import FormInicial from "../components/FormInicial";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/formulario" element={<FormInicial />} />
         <Route path="/inicio" element={<Inicio />} />
         <Route path="*" element={<p>Página no encontrada</p>} />
       </Routes>
