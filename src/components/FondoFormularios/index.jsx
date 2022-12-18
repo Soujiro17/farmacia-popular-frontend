f9d659747412ac479e056cc42dd23bfef49f1d20
lukas <lukas@localhost.localdomain>
2022-12-18 20:28:32 -0300

    Se hizo el formulario de pacientes

--- src/components/FondoFormularios/index.jsx
+++ src/components/FondoFormularios/index.jsx
@@ -1,6 +1,6 @@
 export default function FondoFormularios(props){
     return(
-        <div className="min-h-screen bg-gradient-to-b via-slate-900 from-black to-fuchsia-900  place-items-center">
+        <div className="min-h-screen bg-gradient-to-b via-slate-900 from-black to-fuchsia-900 ">
         {props.children}
         <img src="/img/icono.png" alt="Icono" className="absolute top-[80%] scale-50" />
         </div>
