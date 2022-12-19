b7e178cc49924d0262daaa18c120a0a7576c02c4
Soujiro17 <vicente_tomi@hotmail.com>
2022-12-18 23:16:26 -0300

    react query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AppRoutes from "./routes";
@@ -26,5 +27,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <ToastContainer />
       </UserProvider>
     </AuthProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
 );
